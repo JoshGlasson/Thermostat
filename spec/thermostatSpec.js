30bac56700bb9f03b7b4ca433e8d8e512061381c
Josh Glasson <Josh.Glasson@tesco.com>
2019-04-16 13:56:15 +0100

    added next few tests

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -23,4 +23,30 @@ describe('Thermostat', function(){
       expect(thermostat.temperature()).toBe(19);
     });
   });
+
+  describe('has temperature limits', function(){
+    it('has a minimum of 10', function(){
+      for (var i = 0; i < 14; i++) {
+        thermostat.decrease();
+      }
+      expect(thermostat.temperature()).toEqual(10)
+    });
+    describe('powersave mode on', function(){
+      it('has a maximum of 25', function(){
+        for (var i = 0; i < 10; i++) {
+          thermostat.increase();
+        }
+        expect(thermostat.temperature()).toEqual(25)
+      });
+    });
+    describe('powersave mode off', function(){
+      it('has a maximum of 32', function(){
+        thermostat.togglePowersave();
+        for (var i = 0; i < 15; i++) {
+          thermostat.increase();
+        }
+        expect(thermostat.temperature()).toEqual(32)
+      });
+    });
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function Thermostat(){
-  this._temperature = 20
+  this._MINIMUM_TEMPERATURE = 10;
+  this._temperature = 20;
+  this._powersave = true
 }
 
 Thermostat.prototype.temperature = function() {
@@ -9,9 +11,24 @@ Thermostat.prototype.temperature = function() {
 };
 
 Thermostat.prototype.increase = function() {
+  if(this._powersave === true) {
+    if(this._temperature === 25) {
+      return;
+    }
+  }
+  if(this._temperature === 32) {
+    return;
+  }
   this._temperature++
 };
 
 Thermostat.prototype.decrease = function() {
+  if(this._temperature === this._MINIMUM_TEMPERATURE ) {
+  return;
+    }
   this._temperature--
+};
+
+Thermostat.prototype.togglePowersave = function() {
+  this._powersave = !this._powersave
 };
